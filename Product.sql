d8ee1ff298a502a08cd66f3e6c890e24eda689d5
Phil Fernandez <norcal618@gmail.com>
2020-12-24 00:05:12 -0800

    add sql file

---
+++ Product.sql
@@ -0,0 +1,60 @@
+CREATE TABLE product(
+    id BIGINT AUTO_INCREMENT,
+    name VARCHAR(255),
+    price FLOAT,
+    quantity INT
+);
+
+INSERT INTO product(
+    name, price, quantity
+)
+VALUES(
+    'Sony Car Stereo',
+    500,
+    1234
+);
+
+INSERT INTO product(
+    name, price, quantity
+)
+VALUES(
+    'JBL 10 inch Woofer',
+    89.99,
+    322
+);
+
+INSERT INTO product(
+    name, price, quantity
+)
+VALUES(
+    'Kentwood Tweeter',
+    39.99,
+    8221
+);
+
+INSERT INTO product(
+    name, price, quantity
+)
+VALUES(
+    'JBL 12 inch Woofer',
+    99.99,
+    501
+);
+
+INSERT INTO product(
+    name, price, quantity
+)
+VALUES(
+    'JBL 15 inch Woofer',
+    109.99,
+    121
+);
+
+INSERT INTO product(
+    name, price, quantity
+)
+VALUES(
+    'Clarion Car Stereo',
+    3109.99,
+    23
+);
